124f3745224ac0d6a526b32d33bb89cf3a60f2cc
Warmonk <chrisim@ChrisIM.local>
2019-02-27 23:00:35 -0800

    project 1 to add new python file into local and push to the remote GitHub master account.

---
+++ cap_project1.py
@@ -0,0 +1,14 @@
+'''
+Capstone Project Week 1
+
+This notebook will be used for the capstone project.
+'''
+
+import pandas as pd
+import Numpy as np
+
+print('Hello Capstone Project Course!')
+
+print('added line via the Terminal/emacs')
+
+
